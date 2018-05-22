55b9ae7febb4fe7749e03867e065d8bcb443a050
armenarutyunov <arargur@gmail.com>
2018-05-22 00:45:23 -0400

    Already Created

--- CareerCloud.Pocos/ApplicantEducationsPocos.cs
+++ CareerCloud.Pocos/ApplicantEducationsPocos.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace CareerCloud.Pocos
 {
     [Table("Applicant_Educations")]
