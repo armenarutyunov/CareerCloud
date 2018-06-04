6135575565eb4b9f4b8f603f2711ceb8effe62b0
armenarutyunov <arargur@gmail.com>
2018-06-04 15:45:02 -0400

    First Function "ADD" in class "ApplicantEducationRepository " has described

--- CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -2,6 +2,8 @@
 using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -13,7 +15,27 @@ namespace CareerCloud.ADODataAccessLayer
     {
         public void Add(params ApplicantEducationPoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            int rowsEffected = 0;
+            foreach (ApplicantEducationPoco poco in items)
+            {
+            cmd.CommandText = @"INSERT INTO Applicant_Educations 
+            ( Id, Applicant, Major, Certificate_Diploma, Start_Date, Completion_Date, Completion_Percent) Values 
+            (@Id, @Applicant, @Major, @Certificate_Diploma, @Start_Date, @Completion_Date, @Completion_Percent)";
+                cmd.Parameters.AddWithValue("@Id", poco.Id);
+                cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                cmd.Parameters.AddWithValue("@Major", poco.Major);
+                cmd.Parameters.AddWithValue("@Certificate_Diploma", poco.CertificateDiploma);
+                cmd.Parameters.AddWithValue("@Start_Date", poco.StartDate);
+                cmd.Parameters.AddWithValue("@Completion_Date", poco.CompletionDate);
+                cmd.Parameters.AddWithValue("@Completion_Percent", poco.CompletionPercent);
+
+                conn.Open();
+                rowsEffected += cmd.ExecuteNonQuery();
+                conn.Close();
+            }
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
